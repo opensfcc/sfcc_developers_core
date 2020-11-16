c80cb614fca65be5de7e12e35d2b0c086663a7f5
Peter Schmalfeldt <peter@manifestinteractive.com>
2020-11-16 02:45:22 -0500

    Porting over Previous Work for Dev Drawer

---
+++ cartridges/rvw_dev_drawer/cartridge/scripts/util/devtools.js
@@ -0,0 +1,174 @@
+'use strict';
+
+var serialize = require('./serialize');
+
+/**
+ * Placeholder to store Server Side Debugger Messages
+ */
+var Debugger = {
+    debug: [],
+    error: [],
+    fatal: [],
+    info: [],
+    log: [],
+    warn: []
+};
+
+/**
+ * Generate Stack Trace for Better Debugging
+ * @returns {Object} trace
+ */
+var stackTrace = function() {
+    var trace = {
+        fileName: null,
+        lineNumber: null,
+        stack: null,
+    };
+
+    try {
+        throw new Error('');
+    }
+    catch (error) {
+        trace.stack = error.stack || null;
+    }
+
+    if (trace.stack) {
+        trace.stack = trace.stack.split('\n').map(function (line) { return line.trim(); });
+        trace.stack = trace.stack.splice(trace.stack[0] == 'Error' ? 3 : 2);
+        trace.stack.pop();
+
+        // Get First File from Stack
+        var lastStack = trace.stack[0].split(' ')
+        var fileDetails = lastStack[1];
+        var lineNumber = fileDetails.split(':').pop()
+        var fileName = fileDetails.replace(':' + lineNumber, '')
+
+        trace.fileName = fileName;
+        trace.lineNumber = parseInt(lineNumber);
+        trace.ide = 'http://localhost:60606/target=/' + fileName;
+    }
+
+    return trace;
+}
+
+/**
+ * Add Messages to Debugger
+ * @param {String} method
+ * @param {Object} messages
+ * @param {Object} trace
+ */
+var addMessages = function (method, messages, trace) {
+    for (i in messages) {
+        trace.message = serialize(messages[i]);
+        Debugger[method].push(trace);
+    }
+};
+
+/**
+ * Add Debug Messages to Debugger Queue
+ * @example: dw.system.HookMgr.callHook('rvw.util.devtools', 'debug', myObject);
+ */
+function debug() {
+    try {
+        var messages = Array.prototype.slice.call(arguments, 0) || [];
+        addMessages('debug', messages, stackTrace());
+    } catch (err) {
+        Debugger.fatal.push(err);
+    }
+}
+
+/**
+ * Add Error Messages to Debugger Queue
+ * @example: dw.system.HookMgr.callHook('rvw.util.devtools', 'error', myObject);
+ */
+function error() {
+    try {
+        var messages = Array.prototype.slice.call(arguments, 0) || [];
+        addMessages('error', messages, stackTrace());
+    } catch (err) {
+        Debugger.fatal.push(err);
+    }
+}
+
+/**
+ * Add Info Messages to Debugger Queue
+ * @example: dw.system.HookMgr.callHook('rvw.util.devtools', 'info', myObject);
+ */
+function info() {
+    try {
+        var messages = Array.prototype.slice.call(arguments, 0) || [];
+        addMessages('info', messages, stackTrace());
+    } catch (err) {
+        Debugger.fatal.push(err);
+    }
+}
+
+/**
+ * Add Log Messages to Debugger Queue
+ * @example: dw.system.HookMgr.callHook('rvw.util.devtools', 'log', myObject);
+ */
+function log() {
+    try {
+        var messages = Array.prototype.slice.call(arguments, 0) || [];
+        addMessages('log', messages, stackTrace());
+    } catch (err) {
+        Debugger.fatal.push(err);
+    }
+}
+
+/**
+ * Add Warn Messages to Debugger Queue
+ * @example: dw.system.HookMgr.callHook('rvw.util.devtools', 'warn', myObject);
+ */
+function warn() {
+    try {
+        var messages = Array.prototype.slice.call(arguments, 0) || [];
+        addMessages('warn', messages, stackTrace());
+    } catch (err) {
+        Debugger.fatal.push(err);
+    }
+}
+
+/**
+ * Renders Console Output to Browser
+ * @example: dw.system.HookMgr.callHook('rvw.util.devtools', 'console');
+ */
+function console() {
+    var ISML = require('dw/template/ISML');
+    ISML.renderTemplate('rvw/devtools/console', {
+        Debugger: Debugger
+    });
+}
+
+/**
+ * Renders Debug Drawer in Browser
+ * @example: dw.system.HookMgr.callHook('rvw.util.devtools', 'drawer');
+ */
+function drawer() {
+    var ISML = require('dw/template/ISML');
+
+    // Get Basket Info
+    var BasketMgr = require('dw/order/BasketMgr');
+    var basket = BasketMgr.getCurrentBasket();
+
+    // Get Preferences
+    var Site = require('dw/system/Site');
+    var currentSite = Site.getCurrent();
+    var preferences = Site.getCurrent().getPreferences();
+
+    ISML.renderTemplate('rvw/devtools/drawer', {
+        Debugger: Debugger,
+        basket: serialize(basket),
+        preferences: serialize(preferences),
+        session: serialize(session),
+        site: serialize(currentSite)
+    });
+}
+
+exports.console = console;
+exports.debug = debug;
+exports.drawer = drawer;
+exports.error = error;
+exports.info = info;
+exports.log = log;
+exports.warn = warn;
---
+++ cartridges/rvw_dev_drawer/cartridge/static/default/js/rvw-devtools.js
@@ -0,0 +1,89 @@
+/**
+ * Red Van Workshop
+ * (c) 2020 RedVanWorkshop.com
+ * License: https://github.com/redvanworkshop/rvw_devtools/blob/master/README
+ */
+var RVW_DevTools = (function() {
+    /**
+     * DOM Elements
+     */
+    var $elm = {};
+
+    /**
+     * Handle Prevent Default for Different Conditions
+     * @param {Object} evt
+     */
+    function preventDefault(evt) {
+        evt = evt || window.event;
+        if (evt.preventDefault) {
+            evt.preventDefault();
+        }
+
+        evt.returnValue = false;
+    }
+
+    /**
+     * Bind Events to DOM Elements
+     */
+    function bindEvents() {
+        $elm.drawer = document.querySelector('#rvw-devtools');
+        $elm.drawerButton = document.querySelector('#rvw-devtools button.toggle');
+
+        // Handle Drawer Button Clicks
+        $elm.drawerButton.addEventListener('click', function(evt) {
+            RVW_DevTools.config.drawerOpen = !RVW_DevTools.config.drawerOpen;
+            $elm.drawer.classList = (RVW_DevTools.config.drawerOpen) ? 'open' : 'closed';
+            preventDefault(evt);
+        });
+
+        // Prevent Drawer Scroll Event Passthrough
+        $elm.drawer.addEventListener('mousewheel', function(evt) {
+            //preventDefault(evt);
+        });
+
+        // Prevent Drawer Scroll Event Passthrough on Firefox
+        $elm.drawer.addEventListener('DOMMouseScroll', function(evt) {
+            //preventDefault(evt);
+        });
+
+        // Prevent Drawer Scroll Event Passthrough on Touch Devices
+        $elm.drawer.addEventListener('touchmove', function(evt) {
+            //preventDefault(evt);
+        });
+    }
+
+    /**
+     * Return Dev Tools Object
+     */
+    return {
+        /**
+         * Config Settings
+         */
+        config: {
+            activeTab: 'console',
+            drawerOpen: false
+        },
+
+        /**
+         * Initialize App
+         */
+        init: function() {
+            bindEvents();
+        },
+
+        /**
+         * Switch Dev Tools Tab
+         * @param {String} tab
+         */
+        switchTab: function(tab) {
+            RVW_DevTools.config.activeTab = tab;
+        }
+    }
+})();
+
+/**
+ * Initialize Dev Tools on Page Load
+ */
+window.addEventListener('load', function() {
+    RVW_DevTools.init();
+});
